649f061b8c8d49adb80f4d735105b6b16a21479f
Jonathan Almeida <jonathan.almeida942@gmail.com>
2014-09-03 23:58:33 -0400

    Some hacky code put together in parts
    - Each part exists in this commit, it just needs to be organized and put
      together with some business logic to work.
    - This requires a little more knowledge of nodejs than basic js..
    - TBD

--- app.js
+++ app.js
@@ -5,12 +5,14 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , request = require('request');
+  , request = require('request')
+  , fs = require('fs');
 var app = module.exports = express.createServer();
 
-// API 
+// API
 
 var API_KEY = ""
+var STORAGE_FILE = "./config.json"
 
 // Configuration
 
@@ -31,15 +33,21 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-// Routes
+// Check json in a file
+
+// Is url == new request?
+// while true(), keep checking
+//
 
 app.get('/', routes.index);
 
 app.get('/yo', function(req, res) {
+    console.log("Received a yo. Responding back...");
     data = {"api_token":API_KEY, "username":req.query.username};
   request.post(
-    {url:     "http://api.justyo.co/yo/",
-    form:    { "api_token" : API, "username":req.query.username }
+    {
+        url:     "http://api.justyo.co/yoall/",
+        form:    { "api_token" : API_KEY, "username":req.query.username }
     },
     function (error, response, body) {
       if (!error && response.statusCode == 200) {
@@ -47,6 +55,88 @@ app.get('/yo', function(req, res) {
       }
     });
 });
+
+var originalUrl;
+var newUrl;
+
+readUrlFromFile(STORAGE_FILE, originalUrl);
+
+function send_yo(username) {
+  request.post(
+    {
+        url:     "http://api.justyo.co/yoa/",
+        form:    { "api_token" : API_KEY, "username": username}
+    },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body);
+      }
+    });
+}
+
+function send_yo_all () {
+  // body...
+  request.post(
+    {
+        url:     "http://api.justyo.co/yoall/",
+        form:    { "api_token" : API_KEY}
+    },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body);
+      }
+    });
+}
+
+function readUrlFromFile(file, url) {
+    fs.readFile(file, function(err, data) {
+      if(err) throw err;
+      url = (JSON.parse(data)).url;
+      return (JSON.parse(data)).url;
+    });
+}
+
+
+// read from file initially
+//readJson();
+//readFile();
+//writeFile();
+function readFile() {
+    fs.readFile(STORAGE_FILE, function(err, data) {
+        if(err) throw err;
+        console.log(JSON.parse(data));
+    });
+}
+
+function writeFile() {
+    fs.writeFile(STORAGE_FILE,
+        JSON.stringify({
+            "stuff": "something"
+        }, null, 4),
+        function(err) {
+            if (err) throw err;
+            console.log("SAVED!!!!!!!!!!!!");
+        }
+    );
+}
+
+function readJson() {
+    var url = "http://gdata.youtube.com/feeds/users/sxephil/uploads?max-results=1&alt=json";
+
+    request({
+        url: url,
+        json: true
+    }, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body.feed.entry[0].id.$t);
+            var tmp_url = body.feed.entry[0].id.$t;
+            if (tmp_url != originalUrl) {
+                // send yo
+                // 
+            };
+        }
+    });
+}
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
